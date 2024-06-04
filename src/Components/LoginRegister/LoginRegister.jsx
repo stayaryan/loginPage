4ac4fa93d73ebe692577469c135df58f62ddf503
stayaryan <aryanam2000@gmail.com>
2024-06-04 12:35:10 -0700

    1commit

---
+++ src/Components/LoginRegister/LoginRegister.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoginRegister.css";
+//creating the component
+ const LoginRegister = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+ }
+
+ export default LoginRegister;

659a34c9f9c93c25f4c83f0ee4cfa8b0f7d021c7
stayaryan <aryanam2000@gmail.com>
2024-06-04 18:25:03 -0700

    2ndcommit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import LoginRegister from './Components/LoginRegister/LoginRegister';
 
 function App() {
--- src/Components/LoginRegister/LoginRegister.jsx
+++ src/Components/LoginRegister/LoginRegister.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import "./LoginRegister.css";
+import { FaUser, FaLock } from "react-icons/fa";
+
 //creating the component
  const LoginRegister = () => {
     return (
-        <div>
-            
+        <div className="wrapper">
+            <div className="form-box login">
+                <form action="">
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="text" placeholder="Enter Username" required/>
+                    <FaUser className="icon"/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Enter Password" required/>
+                    <FaLock className="icon"/>
+                </div>
+                <div className="Remember">
+                    <label><input type="checkbox"/>Remember me</label>
+                </div>
+                <div className="loginButton">
+                    <button type="submit">Login</button>
+                </div>
+                <div className="forgot">
+                    <a href="#">Forgot Password?</a>
+                </div>
+                <div className="Register">
+                    <p>Not a member?<a href="#">Register here</a></p>
+                </div>
+                </form>
+            </div>
         </div>
     );
  }
